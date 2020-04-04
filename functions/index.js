a26ec0649f4930f098f74e14dee83f8945ece82d
Andrius <Andriusj6@hotmail.com>
2020-04-04 10:56:20 +0100

    timing and ordering

--- functions/index.js
+++ functions/index.js
@@ -6,26 +6,26 @@ admin.initializeApp()
 const express = require('express')
 const app = express()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello world!')
-})
-
 app.get('/screams', (req, res) => {
   admin
     .firestore()
     .collection('screams')
+    // orders the get request by latests first
+    .orderBy('createdAt', 'desc')
     .get()
     .then(data => {
       let screams = []
       data.forEach(doc => {
-        screams.push(doc.data())
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        })
       })
       return res.json(screams)
     })
-    .catch(err => console.error())
+    .catch(err => console.error(err))
 })
 
 app.post('/screams', (req, res) => {
@@ -38,7 +38,7 @@ app.post('/screams', (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString
   }
 
   admin
