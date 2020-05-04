44a9cc588e6de94ebfcb1058c458981518e8af1f
Andrius <Andriusj6@hotmail.com>
2020-05-04 13:34:39 +0100

    updated schema

--- dbschema.js
+++ dbschema.js
@@ -1,4 +1,16 @@
 let db = {
+  users: [
+    {
+      userId: 'dh345jjh3234lkj4234lf',
+      email: 'user@gmail.com',
+      userHandle: 'user',
+      createdAt: '2020-04-04T09:58:33.518Z',
+      imageUrl: 'https://image.com/imageurl',
+      bio: 'This is a test bio',
+      website: 'https://user.com',
+      location: 'London, UK'
+    }
+  ],
   screams: [
     {
       userHandle: 'user',
