f4dfc3076e3093040822a484608fff5fdb88d6cb
Andrius <Andriusj6@hotmail.com>
2020-05-04 14:05:52 +0100

    added redux data

--- dbschema.js
+++ dbschema.js
@@ -21,3 +21,20 @@ let db = {
     }
   ]
 }
+
+const userDetails = {
+  credentials: {
+    userId: 'dh345jjh3234lkj4234lf',
+    email: 'user@gmail.com',
+    userHandle: 'user',
+    createdAt: '2020-04-04T09:58:33.518Z',
+    imageUrl: 'https://image.com/imageurl',
+    bio: 'This is a test bio',
+    website: 'https://user.com',
+    location: 'London, UK'
+  },
+  likes: [
+    { userHandle: 'user', screamId: '42342khkjj34234kjh234jh545j' },
+    { userHandle: 'user', screamId: '234jhu348h34hk3j4878ghjjhi7' }
+  ]
+}
