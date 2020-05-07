3faa00a175e2a821615fcb12aa4720028cc6d234
Andrius <Andriusj6@hotmail.com>
2020-05-07 13:30:30 +0100

    added comment count incrementation

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -119,6 +119,9 @@ exports.postScreamComment = (req, res) => {
       if (!doc.exists) {
         return res.status(404).json({ error: 'Scream not found' })
       }
+      return doc.ref.update({ commentCount: doc.data().commentCount + 1 })
+    })
+    .then(() => {
       return db.collection('comments').add(newComment)
     })
     .then(() => {
