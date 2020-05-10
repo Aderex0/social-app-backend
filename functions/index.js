9b8ddc3c02994239d977b35ac4579f78f53c6dc7
Andrius <Andriusj6@hotmail.com>
2020-05-10 12:31:41 +0100

    fixes and delte notifications

--- functions/index.js
+++ functions/index.js
@@ -54,7 +54,7 @@ exports.createNotificationOnLike = functions
       .then(doc => {
         if (
           doc.exists &&
-          doc.data().userHandle !== nspashot.data().userHandle
+          doc.data().userHandle !== snapshot.data().userHandle
         ) {
           return db.doc(`/notifications/${snapshot.id}`).set({
             recipient: doc.data().userHandle,
@@ -94,7 +94,7 @@ exports.createNotificationOnComment = functions
       .then(doc => {
         if (
           doc.exists &&
-          doc.data().userHandle !== nspashot.data().userHandle
+          doc.data().userHandle !== snapshot.data().userHandle
         ) {
           return db.doc(`/notifications/${snapshot.id}`).set({
             recipient: doc.data().userHandle,
@@ -109,4 +109,65 @@ exports.createNotificationOnComment = functions
       .catch(err => {
         console.error(err)
       })
+  })
+
+exports.onUserImageChange = functions
+  .region('europe-west1')
+  .firestore.document('/users/{userId}')
+  .onUpdate(change => {
+    console.log(change.before.data())
+    console.log(change.after.data())
+
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+      console.log('image changed')
+      const batch = db.batch()
+      return db
+        .collection('screams')
+        .where(`userHandle`, '==', change.before.data().userHandle)
+        .get()
+        .then(data => {
+          data.forEach(doc => {
+            const scream = db.doc(`/screams/${doc.id}`)
+            batch.update(scream, { userImage: change.after.data().imageUrl })
+          })
+          return batch.commit()
+        })
+    } else return true
+  })
+
+exports.onScreamDelete = functions
+  .region('europe-west1')
+  .firestore.document('/screams/{screamId}')
+  .onDelete((snapshot, context) => {
+    const screamId = context.params.screamId
+    const batch = db.batch()
+    return db
+      .collection('comments')
+      .where('screamId', '==', screamId)
+      .get()
+      .then(data => {
+        data.forEach(doc => {
+          batch.delete(db.doc(`/comments/${doc.id}`))
+        })
+        return db
+          .collection('likes')
+          .where('screamId', '==', screamId)
+          .get()
+      })
+      .then(data => {
+        data.forEach(doc => {
+          batch.delete(db.doc(`/likes/${doc.id}`))
+        })
+        return db
+          .collection('notifications')
+          .where('screamId', '==', screamId)
+          .get()
+      })
+      .then(data => {
+        data.forEach(doc => {
+          batch.delete(db.doc(`/notifications/${doc.id}`))
+        })
+        return batch.commit()
+      })
+      .catch(err => console.error(err))
   })
